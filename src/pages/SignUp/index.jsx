75a8a7d840c7d47bdde156c64355fa1c47919901
dev-cardenas <dev.cardenas@gamil.com>
2019-10-10 23:37:50 -0300

    use unform rocketseat

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Logo-go-barber" />
-      <form action="">
-        <input placeholder="Nombre completo" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nombre completo" />
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Password" />
 
         <button type="submit">Crear cuenta</button>
         <Link to="/">Ya tengo una cuenta</Link>
-      </form>
+      </Form>
     </>
   );
 }
