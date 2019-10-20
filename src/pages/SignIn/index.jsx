c1bdd053e1465f9993c3542b7045595a61a7d754
dev-cardenas <dev.cardenas@gamil.com>
2019-10-20 11:58:57 -0300

    validations add

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Escriba un email valido')
+    .required('El email es obligatorio'),
+  password: Yup.string().required('La contraseña es obligatoria'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -12,9 +20,9 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="Logo-go-barber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Email" />
-        <Input name="password" type="password" placeholder="Password" />
+        <Input name="password" type="password" placeholder="Contraseña" />
 
         <button type="submit">Acceder</button>
         <Link to="/register">Crear cuenta gratuita</Link>
