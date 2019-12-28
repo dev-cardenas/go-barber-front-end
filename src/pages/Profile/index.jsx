7ea4653265f3bd27c681d2408b7572a4bfcaa941
dev-cardenas <dev.cardenas@gamil.com>
2019-12-27 21:27:21 -0300

    add profile styles

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nombre completo" />
+        <Input name="email" type="email" placeholder="Correo" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Su vieja cotrasenha"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Su nueva contrasenha"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme su contrasenha"
+        />
+
+        <button type="submit">Actualizar perfil</button>
+      </Form>
+      <button type="button">Salir de GoBarber</button>
+    </Container>
+  );
 }
