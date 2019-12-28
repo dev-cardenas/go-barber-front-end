57a573db7d2ebeee61c689ff4c5af00b75fa567d
dev-cardenas <dev.cardenas@gamil.com>
2019-12-27 23:28:17 -0300

    create saga and actions for user, and add function for update perfil, and img profile

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,30 @@
-import { all } from 'redux-saga/effects';
+import { takeLastest, call, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = Object.assign(
+      { name, email, avatar_id },
+      rest.oldPassword ? rest : {}
+    );
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil actualizado');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Error al actualizar perfil');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([
+  takeLastest('@user/UPDATE_PROFILE_REQUEST', updateProfile),
+]);
