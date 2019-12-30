f3fc7cad9e7a8399c67758d2085c229890a5d631
dev-cardenas <dev.cardenas@gamil.com>
2019-12-29 23:09:33 -0300

    avances in dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <div>jop</div>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Mayo</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time>
+          <strong past>08:00</strong>
+          <strong>Diego</strong>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <strong>Diego</strong>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <strong>Diego</strong>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
